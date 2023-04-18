70fb14416c0d9c2b75155aaf51cc9d9ce4dc1c37
eoneee <sangeon0412@gmail.com>
2023-04-18 16:16:41 +0900

    Timestamped 적용

--- springlv1/src/main/java/com/example/springlv1/Springlv1Application.java
+++ springlv1/src/main/java/com/example/springlv1/Springlv1Application.java
@@ -2,7 +2,9 @@ package com.example.springlv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Springlv1Application {
 
--- springlv1/src/main/java/com/example/springlv1/entity/Crud.java
+++ springlv1/src/main/java/com/example/springlv1/entity/Crud.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 @Getter
 @Entity
 @NoArgsConstructor
-public class Crud {
+public class Crud extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
